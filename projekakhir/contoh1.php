0939406615cd4d33e399e6c6eacdf27795ea1e5f
LukasKurniawan <102298857+LukasKurniawan@users.noreply.github.com>
2022-06-22 12:21:28 +0700

    Add files via upload

---
+++ projekakhir/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo" Nama saya Lukas Kurniawan
+			saya tinggal di Jakarta Barat
+			olahraga favorit saya adalah volly";
+	}
+}
